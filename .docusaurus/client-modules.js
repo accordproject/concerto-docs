bdaebc642383ec899c04904d4cf650e731bbe8fa
Dan Selman <danscode@selman.org>
2022-12-13 12:30:39 +0000

    chore remove build files
    Signed-off-by: Dan Selman <danscode@selman.org>

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/dan.selman/dev/concerto-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/dan.selman/dev/concerto-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/dan.selman/dev/concerto-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/dan.selman/dev/concerto-docs/src/css/custom.css'),
-];
